70c6f2683dac6aba1f086ad92e09007deb9e9701
Danilo Djokic <djokicd@outlook.com>
2024-03-27 10:45:17 +0100

    Modified test to support negative side length

--- test_mnogougao.py
+++ test_mnogougao.py
@@ -1,9 +1,11 @@
 from mnogougao import *
 import pytest 
  
-@pytest.mark.parametrize("a", np.linspace(0, 10, 11))
+@pytest.mark.parametrize("a", np.linspace(-10, 10, 21))
 def test_kvadrat(a):
-    k = Kvadrat(a)
-    
-    assert k.povrsina() == a**2
-    assert k.obim() == 4*a
+    try:
+        k = Kvadrat(a)
+        assert k.povrsina() == a**2
+        assert k.obim() == 4*a
+    except ValueError as e:
+        assert a < 0
