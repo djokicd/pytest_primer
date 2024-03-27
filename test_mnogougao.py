9e8ee6e9373d9d116f0250906cd4d801b149cb2d
Danilo Djokic <djokicd@outlook.com>
2024-03-27 11:00:17 +0100

    Added PravougliTrougao tests

--- test_mnogougao.py
+++ test_mnogougao.py
@@ -9,3 +9,10 @@ def test_kvadrat(a):
         assert k.obim() == 4*a
     except ValueError as e:
         assert a < 0
+
+@pytest.mark.parametrize("a, b", [ (a,b) for a in np.linspace(0, 1, 20) for b in (0, 1, 20) ] )
+def test_trougao(a, b):
+    t = PravougliTrougao(a, b)
+    assert t.povrsina() == a*b/2
+    assert np.abs(t.obim() -  (a + b + np.sqrt(a**2 + b**2) ) ) < 1e-6
+
